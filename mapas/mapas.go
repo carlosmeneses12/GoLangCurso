2c6cd89f85d170a54a65623ba331d61e0eab9cd5
carlos <carlos.nosk@gmail.com>
2025-03-21 12:46:53 -0300

    map

--- mapas/mapas.go
+++ mapas/mapas.go
@@ -6,5 +6,29 @@ import (
 
 func MostrarMapas() {
 	paises := make(map[string]string)
+
+	paises["Mexico"] = "D .F . M"
+	paises["Argentina"] = " Buenos Aires"
+
 	fmt.Println(paises)
+	fmt.Println(paises["Argentina"]) //al llamar al elemento por su clave solo muestra el valor "Buenos aires"
+
+	campeonato := map[string]int{
+		"Barcelona ": 39,
+		"U de chile": 37,
+		"colo-colo":  -3,
+		"Catolica":   -6,
+	}
+
+	fmt.Println(campeonato)
+
+	/*for equipo, puntaje := range campeonato {
+		fmt.Printf("equipo %s, tiene un puntaje de %d \n", equipo, puntaje)
+	}*/
+
+	delete(campeonato, "Barcelona ")
+	fmt.Println(campeonato)
+
+	puntaje, existe := campeonato["U de chile"]
+	fmt.Printf("El puntaje capturado es %d, y el equipo existe = %t", puntaje, existe)
 }
